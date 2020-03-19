8e340a54c4fb08ffefb90a25a3b4431c2fe5767f
Alysha Columbres <alysha.columbres@obf.ateneo.edu>
2020-03-19 17:50:31 +0800

    Created the functional test and project

---
+++ functional_test.py
@@ -0,0 +1,19 @@
+import unittest
+
+from selenium import webdriver
+
+
+class NewVisitorTest(unittest.TestCase):
+
+	def setUp(self):
+		self.browser = webdriver.Firefox()
+
+	def tearDown(self):
+		self.browser.quit()
+
+	# initial testing
+	def test_can_open_homepage(self):
+		self.browser.get('http://localhost:8000')
+
+if __name__ == '__main__':
+	unittest.main(warnings = 'ignore')
